470feee0c6e374cdfd1f5e90207348b8422bbf8f
alexmerlo0623 <a.merlo@ufl.edu>
2026-03-01 14:18:09 -0500

    Changed text

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -16,8 +16,8 @@ export const AboutSection = () => {
             </h3>
 
             <p className="text-muted-foreground">
-              With over 5 years of experience in web development, I specialize
-              in creating responsive, accessible, and performant web
+              With 2 years of experience in web development, I specialize
+              in creating responsive and performant web
               applications using modern technologies.
             </p>
 
@@ -35,11 +35,11 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/Resume.pdf"
+                href="/Alexs Software Dev Resume.pdf"
                 download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
-                Download CV
+                Download Resume
               </a>
 
             </div>
